4d258000be74084b300db52ec885b721364c6566
Vasiliy Novikov <novikoffvasiliy@yandex.ru>
2023-02-18 23:37:04 +0300

    add type hiting

--- test_file.py
+++ test_file.py
@@ -1,3 +1,13 @@
 print('This is a file from GitHub repository')
 
 print('set-url ssh')
+
+
+def plus_two(num: int) -> int:
+    print('start function plus_two')
+    return num + 2
+
+
+print(
+    plus_two(3)
+)
