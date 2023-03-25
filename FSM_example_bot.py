2268778fac58585b398f8c7e3ef3f17309d4dd00
Vasiliy Novikov <novikoffvas@mail.ru>
2023-03-25 14:47:14 +0300

    added new example

---
+++ FSM_example_bot.py
@@ -0,0 +1,8 @@
+"""
+Пример использования состояний в разные моменты взаимодействия с ботом.
+
+Для наглядности в одном файле.
+"""
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command, StateFilter, Text
--- test_file.py
+++ test_file.py
@@ -1,9 +1,16 @@
-from math import factorial
+def chunked(s, n):
+    li: list = []
+    for i in range(len(s)):
+        if not li:
+            li.append([s[i]])
+        elif i % n != 0:
+            li[-1].extend([s[i]])
+        else:
+            li.append([s[i]])
+    return li
 
+
+s = input().split()
 n = int(input())
-result = []
 
-for i in range(n + 1):
-    result.append(factorial(n) // (factorial(i) * factorial(n - i)))
-
-print(result)
+print(chunked(s, n))
