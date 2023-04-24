76d04cbd656ac9e274ed6353148ac30a16917664
Vasiliy Novikov <novikoffvas@mail.ru>
2023-04-24 15:23:48 +0300

    implement saturdays_between_two_dates function

--- test_file.py
+++ test_file.py
@@ -1,8 +1,30 @@
-from datetime import date
+from datetime import date, timedelta
 
-dates = [date(2010, 9, 28), date(2017, 1, 13), date(2009, 12, 25), date(2010, 2, 27), date(2021, 10, 11),
-         date(2020, 3, 13), date(2000, 7, 7), date(1999, 4, 14), date(1789, 11, 19), date(2013, 8, 21),
-         date(1666, 6, 6), date(1968, 5, 26)]
 
-for d in dates:
-    print(f'{d.year}-Q{(d.month - 1) // 3 + 1}')
+def saturdays_between_two_dates(start, end):
+    saturday_count = 0
+    if end < start:
+        start, end = end, start
+    while start <= end:
+        if start.isoweekday() == 6:
+            saturday_count += 1
+            start += timedelta(7)
+            continue
+        start += timedelta(1)
+    return saturday_count
+
+
+date1 = date(2021, 11, 1)
+date2 = date(2021, 11, 22)
+
+print(saturdays_between_two_dates(date1, date2))
+
+date1 = date(2020, 7, 26)
+date2 = date(2020, 7, 2)
+
+print(saturdays_between_two_dates(date1, date2))
+
+date1 = date(2018, 7, 13)
+date2 = date(2018, 7, 13)
+
+print(saturdays_between_two_dates(date1, date2))
