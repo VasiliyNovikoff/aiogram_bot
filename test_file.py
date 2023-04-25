6f5b97609f6027f54f90a2f91f07201eb8b76435
Vasiliy Novikov <novikoffvas@mail.ru>
2023-04-25 12:29:05 +0300

    example of date.fromisoformat

--- test_file.py
+++ test_file.py
@@ -1,30 +1,6 @@
-from datetime import date, timedelta
+from datetime import date
 
 
-def saturdays_between_two_dates(start, end):
-    saturday_count = 0
-    if end < start:
-        start, end = end, start
-    while start <= end:
-        if start.isoweekday() == 6:
-            saturday_count += 1
-            start += timedelta(7)
-            continue
-        start += timedelta(1)
-    return saturday_count
-
-
-date1 = date(2021, 11, 1)
-date2 = date(2021, 11, 22)
-
-print(saturdays_between_two_dates(date1, date2))
-
-date1 = date(2020, 7, 26)
-date2 = date(2020, 7, 2)
-
-print(saturdays_between_two_dates(date1, date2))
-
-date1 = date(2018, 7, 13)
-date2 = date(2018, 7, 13)
-
-print(saturdays_between_two_dates(date1, date2))
+my_date = date.fromisoformat('2222-12-09')
+print(my_date)
+print(type(my_date))
