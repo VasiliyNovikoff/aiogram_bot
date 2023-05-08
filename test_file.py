d9bba82a43272223e773b01a508fb49a88152884
Vasiliy Novikov <novikoffvas@mail.ru>
2023-05-08 15:15:51 +0300

    employments

--- test_file.py
+++ test_file.py
@@ -1,9 +1,20 @@
 from datetime import datetime
 
 
-dates = [datetime.strptime(i, '%d.%m.%Y') for i in input().split(' ')]
-days = []
-for i in range(1, len(dates)):
-    day = abs(dates[i] - dates[i - 1])
-    days.append(day.days)
-print(days)
+n = int(input())
+employments = [input() for _ in range(n)]
+pattern = '%d.%m.%Y'
+employments_dict: dict = {}
+
+for employment in employments:
+    date_key = datetime.strptime(employment.split(' ')[-1], pattern)
+    date_value = employment.split(' ')[0] + ' ' + employment.split(' ')[1]
+    employments_dict[date_key] = employments_dict.get(date_key, 0) + 1
+
+new_employments_list = sorted(employments_dict.items(), key=lambda k: (-k[1], k[0]))
+
+for i in range(len(new_employments_list)):
+    if new_employments_list[i][1] == new_employments_list[0][1]:
+        print(new_employments_list[i][0].strftime(pattern))
+    else:
+        break
