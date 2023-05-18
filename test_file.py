e170470767d368fe67797fe2da1efbef8a141ae4
Vasiliy Novikov <novikoffvas@mail.ru>
2023-05-18 15:24:15 +0300

    sys task

--- test_file.py
+++ test_file.py
@@ -1,20 +1,6 @@
-from datetime import date
-import calendar
+import sys
 
 
-def get_all_thursday(year):
-    date_list = []
-    for month in range(1, 13):
-        month_cal = calendar.monthcalendar(year, month)
-        first_thursday = month_cal[0][3]
-        third_thursday = month_cal[2][3] if first_thursday != 0 else month_cal[3][3]
-        date_list.append(date(year, month, third_thursday))
-    return print_date(date_list)
-
-
-def print_date(date_list: list[date]):
-    for dt in date_list:
-        print(dt.strftime('%d.%m.%Y'))
-
-
-get_all_thursday(int(input()))
+socks_counts = list(map(str.strip, sys.stdin))
+players = ['Дима', 'Анри']
+print(players[(len(socks_counts) + int(socks_counts[-1])) % 2])
