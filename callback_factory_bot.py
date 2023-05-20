a0e18a30e7e798fa259ab88943b27630bf51dac9
Vasiliy Novikov <novikoffvas@mail.ru>
2023-05-20 18:11:55 +0300

    простенький бот, который демонстрирует фабрику коллбэков

--- callback_factory_bot.py
+++ callback_factory_bot.py
@@ -3,6 +3,7 @@ from aiogram.filters.callback_data import CallbackData
 from aiogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
                            Message, CallbackQuery)
 from aiogram.filters import CommandStart
+from aiogram import F
 
 from config_data.config import Config, load_config
 
@@ -48,12 +49,33 @@ async def process_start_command(message: Message):
                          reply_markup=markup)
 
 
-# Этот хэндлер будет срабатывать на нажатие любой инлайн кнопки
+# Этот хэндлер будет срабатывать на нажатие любой инлайн-кнопки
+# и отправлять в чат форматированный ответ с данными из callback_data
+@dp.callback_query(GoodsCallbackFactory.filter())
+async def process_category_press(callback: CallbackQuery,
+                                 callback_data: GoodsCallbackFactory):
+    await callback.message.answer(
+        text=f'Категория товаров: {callback_data.category_id}\n'
+             f'Подкатегория товаров: {callback_data.subcategory_id}\n'
+             f'Товар: {callback_data.item_id}')
+    await callback.answer()
+
+
+'''# Используем магический фильтр, чтобы поймать нажатие
+# на кнопку Категория 1
+@dp.callback_query(GoodsCallbackFactory.filter(F.category_id == 1))
+async def process_category_press(callback: CallbackQuery,
+                                 callback_data: GoodsCallbackFactory):
+    await callback.message.answer(text=callback_data.pack())
+    await callback.answer()'''
+
+
+'''# Этот хэндлер будет срабатывать на нажатие любой инлайн кнопки
 # и печатать объект в терминал
 @dp.callback_query()
 async def process_any_inline_button_press(callback: CallbackQuery):
     print(callback.json(indent=4, exclude_none=True))
-    await callback.answer()
+    await callback.answer()'''
 
 
 if __name__ == '__main__':
