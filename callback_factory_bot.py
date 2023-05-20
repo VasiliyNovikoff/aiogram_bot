411d88947e8944d36237785837402c6c21966058
Vasiliy Novikov <novikoffvas@mail.ru>
2023-05-20 15:49:12 +0300

    фабрика коллбэков и формирование callback_data

---
+++ callback_factory_bot.py
@@ -0,0 +1,60 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
+                           Message, CallbackQuery)
+from aiogram.filters import CommandStart
+
+from config_data.config import Config, load_config
+
+
+config: Config = load_config()
+BOT_TOKEN = load_config().tg_bot.token
+
+# Инициализируем бот и диспетчер
+bot: Bot = Bot(token=BOT_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+
+# Создаем свой класс фабрики коллбэков, указывая префис,
+# а также структуру callback_data
+class GoodsCallbackFactory(CallbackData, prefix='goods', sep='|'):
+    category_id: int
+    subcategory_id: int
+    item_id: int
+
+
+# Создаем объекты кнопок с приминением фабрики коллбэков
+button_1: InlineKeyboardButton = InlineKeyboardButton(text='Категория 1',
+                                                      callback_data=GoodsCallbackFactory(
+                                                          category_id=1,
+                                                          subcategory_id=0,
+                                                          item_id=0).pack())
+
+button_2: InlineKeyboardButton = InlineKeyboardButton(text='Категория 2',
+                                                      callback_data=GoodsCallbackFactory(
+                                                          category_id=2,
+                                                          subcategory_id=0,
+                                                          item_id=0).pack())
+
+# Создаем объект клавиатуры, добавляя в список списки с кнопками
+markup: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2]])
+
+
+# Этот хэндлер будет срабатывать на комманду /start
+# и отправлять пользователю сообщение с клавиатурой
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text='Вот такая клавиатура',
+                         reply_markup=markup)
+
+
+# Этот хэндлер будет срабатывать на нажатие любой инлайн кнопки
+# и печатать объект в терминал
+@dp.callback_query()
+async def process_any_inline_button_press(callback: CallbackQuery):
+    print(callback.json(indent=4, exclude_none=True))
+    await callback.answer()
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
