0e9ef78f01693340dcd32c2cf5b140dbd519ef32
Vasiliy Novikov <novikoffvas@mail.ru>
2023-05-25 15:04:42 +0300

    создание csv с сортировкой по дате и email

--- test_file.py
+++ test_file.py
@@ -1,10 +1,21 @@
 import csv
+from datetime import datetime
 
 
-with open('/Users/vasiliy/Downloads/titanic.csv', encoding='utf-8') as file:
-    reader = list(csv.DictReader(file, delimiter=';'))
+with open('/Users/vasiliy/Downloads/name_log.csv', encoding='utf-8') as file:
+    reader = list(csv.DictReader(file))
 
-reader.sort(key=lambda item: item['sex'], reverse=True)
-for passenger in reader:
-    if int(passenger['survived']) and float(passenger['age']) < 18:
-        print(passenger['name'])
+pattern = '%d/%m/%Y %H:%M'
+reader.sort(key=lambda item: (item['email'], -datetime.strptime(item['dtime'], pattern).timestamp()))
+
+new_reader = []
+email_list = []
+for user in reader:
+    if user['email'] not in email_list:
+        new_reader.append(user)
+        email_list.append(user['email'])
+
+with open('new_name_log.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=reader[0].keys())
+    writer.writeheader()
+    writer.writerows(new_reader)
