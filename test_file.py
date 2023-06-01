e5c5c6d6acc3e5787a532a51c3ded805309e9945
Vasiliy Novikov <novikoffvas@mail.ru>
2023-06-01 18:59:24 +0300

    zip

--- test_file.py
+++ test_file.py
@@ -1,16 +1,4 @@
-import json
+import zipfile
 
 
-with open('/Users/vasiliy/Downloads/food_services.json', encoding='utf-8') as file:
-    shops = json.load(file)
-
-districts_count = {}
-shops_count = {}
-for shop in shops:
-    districts_count[shop['District']] = districts_count.get(shop['District'], 0) + 1
-    shops_count[shop['OperatingCompany']] = shops_count.get(shop['OperatingCompany'], 0) + 1
-
-del shops_count['']
-
-print(*max(districts_count.items(), key=lambda x: x[1]), sep=': ')
-print(*max(shops_count.items(), key=lambda x: x[1]), sep=': ')
+print(zipfile.is_zipfile('/Users/vasiliy/Downloads/test.zip'))
