57cc4254cf57632a067b51d09e4c80d6dab4406e
Vasiliy Novikov <novikoffvas@mail.ru>
2023-06-10 18:39:52 +0300

    pickle

--- test_file.py
+++ test_file.py
@@ -1,4 +1,12 @@
-import zipfile
+import sys
+import pickle
 
 
-print(zipfile.is_zipfile('/Users/vasiliy/Downloads/test.zip'))
+name_func, *args = [arg.strip('\n') for arg in sys.stdin]
+
+with open(name_func, 'rb') as pickle_file:
+    pickle_func = pickle.load(pickle_file)
+
+print(pickle_func(*args))
+print(name_func)
+print(args)
